8f42d5a387984c380f5fd97379757cc13256eefb
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2021-06-23 14:19:25 -0300

    Fixed some things on mix.exs to first release

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule PhoenixReactify.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:cli_spinners, "~> 0.1.0"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
