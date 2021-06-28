46dc92e8fd46d5183133d4296a016e758a863735
João Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2021-06-28 11:28:13 -0300

    Fixed test files

---
+++ test/helpers_test.exs
@@ -0,0 +1,5 @@
+defmodule PhoenixReactify.HelpersTest do
+  use ExUnit.Case
+
+  doctest PhoenixReactify.Helpers.Npm
+end
--- test/phoenix_reactify_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PhoenixReactifyTest do
-  use ExUnit.Case
-  doctest PhoenixReactify
-
-  test "greets the world" do
-    assert PhoenixReactify.hello() == :world
-  end
-end
