7db69f7597376f5c8c2ccbd6c8b25f8cff8e0193
Joao Augusto Perin de Souza <joaoaugustoperin@gmail.com>
2021-10-03 22:00:07 -0300

    fix version mismatch issues

--- lib/helpers/helper.ex
+++ lib/helpers/helper.ex
@@ -1,6 +1,31 @@
 defmodule PhoenixReactify.Helper do
   defmacro __using__(_opts) do
     quote do
+      defp clear_left_input(value) do
+        cond do
+          is_nil(value) ->
+            0
+
+          !Regex.match?(~r/^\d+$/, value) ->
+            0
+
+          !is_nil(value) ->
+            String.to_integer(value)
+
+          true ->
+            0
+        end
+      end
+
+      defp clear_right_input(value) do
+        cond do
+          is_nil(value) -> 0
+          !Regex.match?(~r/^\d+$/, value) -> 0
+          String.last(value) == "+" -> String.to_integer(Enum.at(String.split(value, "+"), 0))
+          true -> String.to_integer(value)
+        end
+      end
+
       @descriptor String.split(to_string(__MODULE__), ".") |> Enum.reverse() |> Enum.at(0)
 
       def compatible?(version \\ "") do
@@ -13,22 +38,9 @@ defmodule PhoenixReactify.Helper do
 
         try do
           for index <- 0..max(length(split_param_arr), length(split_supported_arr)) do
-            left =
-              if !is_nil(Enum.at(split_param_arr, index)),
-                do: String.to_integer(Enum.at(split_param_arr, index)),
-                else: 0
+            left = clear_left_input(Enum.at(split_param_arr, index))
 
-            right =
-              if !is_nil(Enum.at(split_supported_arr, index)),
-                do:
-                  if(String.last(Enum.at(split_supported_arr, index)) == "+",
-                    do:
-                      String.to_integer(
-                        Enum.at(String.split(Enum.at(split_supported_arr, index), "+"), 0)
-                      ),
-                    else: String.to_integer(Enum.at(split_supported_arr, index))
-                  ),
-                else: 0
+            right = clear_right_input(Enum.at(split_supported_arr, index))
 
             result = left >= right
 
